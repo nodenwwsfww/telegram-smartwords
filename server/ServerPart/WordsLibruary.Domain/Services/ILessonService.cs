bab6aaa5e46d739167b87a8f35f5cf0b6f00ca0b
BBoyDragon <kirill.kirsanov2000@gmail.com>
2022-03-28 19:33:27 +0300

    primal server realization uploaded

---
+++ server/ServerPart/WordsLibruary.Domain/Services/ILessonService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsLibruary.Domain.Services
+{
+    public interface ILessonService
+    {
+       public LessonModel Create(LessonModel lesson);
+    }
+}
